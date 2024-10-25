293690d92d6284c6a42f8ea84bc958caf72dae4d
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-25 17:00:15 +0900

    Add files via upload

---
+++ 53zaea.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineage/update/240327/bg_main.mp4#t=0.1";
+        document.getElementById('vd').load();
+    }
+});
+
